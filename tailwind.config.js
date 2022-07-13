5e892a0fc26d269bf2db7f756f8e51eca0e6fbd7
nephydecode <nephydecode@gmail.com>
2022-07-13 18:53:55 +0800

    chore: define screen size in pixels

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    sm: '640px',
+    // => @media (min-width: 640px) { ... }
+
+    md: '760px',
+    // => @media (min-width: 760px) { ... }
+
+    lg: '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    '2xl': '1536px',
+    // => @media (min-width: 1536px) { ... }
+
   },
   plugins: [],
 }
