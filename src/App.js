a78f09169fb52175d03383cd352960f251add9ef
nephydecode <nephydecode@gmail.com>
2022-07-14 15:07:35 +0800

    feat: add Pricing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './components/About';
 import AllInOne from './components/AllInOne';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Pricing from './components/Pricing';
 import Support from './components/Support';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <About/>
     <Support/>
     <AllInOne/>
+    <Pricing/>
     </>
   );
 }
