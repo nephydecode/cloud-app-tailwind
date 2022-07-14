833febd368830d3ad11fada7966311065ac60a1e
nephydecode <nephydecode@gmail.com>
2022-07-14 16:21:25 +0800

    feat: scroll to section from navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
-import {MenuIcon, XIcon} from '@heroicons/react/outline';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 const Navbar = () => {
 
     const [nav, setNav] = useState(false);
     const handleNavClick = () => setNav(!nav);
+
+    const handleClose = () => setNav(!nav);
 
     return ( 
         <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
@@ -16,11 +19,11 @@ const Navbar = () => {
                         /* sm md lg xl sizes are also defined in tailwind.config.js */
                     }
                     <ul className='hidden md:flex'>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Support</li>
-                        <li>Platforms</li>
-                        <li>Pricing</li>
+                        <li><Link to="home" smooth={true} offset={0} duration={500}>Home</Link></li>
+                        <li><Link to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+                        <li><Link to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
+                        <li><Link to="platform" smooth={true} offset={-100} duration={500}>Platform</Link></li>
+                        <li><Link to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
                     </ul>
                 </div>
                 <div>
@@ -35,11 +38,11 @@ const Navbar = () => {
             </div>
             
             <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
-                <li className='border-b-2 border-zinc-300 w-full'>Home</li>
-                <li className='border-b-2 border-zinc-300 w-full'>About</li>
-                <li className='border-b-2 border-zinc-300 w-full'>Support</li>
-                <li className='border-b-2 border-zinc-300 w-full'>Pricing</li>
-                <li className='border-b-2 border-zinc-300 w-full'>Platforms</li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose } tso="home" smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose } to="about" smooth={true} offset={-200} duration={500}>About</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose } to="support" smooth={true} offset={-50} duration={500}>Support</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose } to="platform" smooth={true} offset={-100} duration={500}>Platform</Link></li>
+                <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose } to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link></li>
                 <div className='flex flex-col my-4'>
                     <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign In</button>
                     <button className='px-8 py-3'>Register</button>
